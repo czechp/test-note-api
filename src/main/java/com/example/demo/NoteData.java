8ee67e4e7fbc6ce4ac02d95d941966f4f15d9800
czechp <webcoderc@gmail.com>
2021-08-28 14:54:14 +0200

    Add more note to test

--- src/main/java/com/example/demo/NoteData.java
+++ src/main/java/com/example/demo/NoteData.java
@@ -6,8 +6,16 @@ import java.util.List;
 
 class NoteData {
     static List<Note> notes = new ArrayList<>(Arrays.asList(
-            new Note(1L, "First title", "First content"),
-            new Note(2L, "Second title", "Second content"),
-            new Note(3L, "Third title", "Third content")
+            new Note(1L, "Example title", "Example content"),
+            new Note(2L, "Example title", "Example content"),
+            new Note(3L, "Example title", "Example content"),
+            new Note(4L, "Example title", "Example content"),
+            new Note(5L, "Example title", "Example content"),
+            new Note(6L, "Example title", "Example content"),
+            new Note(7L, "Example title", "Example content"),
+            new Note(8L, "Example title", "Example content"),
+            new Note(9L, "Example title", "Example content"),
+            new Note(10L, "Example title", "Example content"),
+            new Note(1L, "Example title", "Example content")
     ));
 }
